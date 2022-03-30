c163d46f58eadc328d8d272e5d1da31395d7d934
oukrirou <ayouboukrirou@gmail.com>
2022-03-30 14:01:37 +0000

    by me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import Signin from './Compenents/signin';
 import React from 'react';
 import Navbar from "./Compenents/navbar"
+import Sidebar from './Compenents/sidebar';
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import Dashboards from './Compenents/Dashboards';
 // import Prefernces from './Compenents/Prefernces';
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
        {/* <Signin></Signin> */}
        <Navbar/>
+       <Sidebar/>
     </div>
   );
 }
---
+++ my-app/src/Compenents/sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import style from "../Styles/sidebar.module.css"
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ApprovalIcon from '@mui/icons-material/Approval';
+
+
+const Sidebar = () => {
+    return(
+        <div className={style.sidebar}>
+            <div className={style.options}>
+                <div>
+                    <span><DashboardOutlinedIcon/></span>
+                    <span className={style.oplabel} >Dashboard</span>
+                </div>
+                <div>
+                    <span><InsightsOutlinedIcon/></span>
+                    <span className={style.oplabel} >Statistic</span>
+                </div>
+                <div>
+                    <span><ApprovalIcon/></span>
+                    <span className={style.oplabel} >Approval</span>
+                </div>
+            </div>
+            <div className={style.setting}>
+                <span><SettingsOutlinedIcon/></span>
+                <span className={style.oplabel}>Setting</span>
+            </div>
+        </div>
+    ) ;
+}
+
+// #endregion
+
+export default Sidebar;
