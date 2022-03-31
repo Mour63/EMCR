383ffdfc32e1f7053c2c18a0aa4c417fd9fa6530
oukrirou <ayouboukrirou@gmail.com>
2022-03-31 15:54:56 +0000

    by me

--- my-app/src/Compenents/Dashboards.js
+++ my-app/src/Compenents/Dashboards.js
@@ -2,61 +2,68 @@ import React from 'react';
 import Sidebar from './sidebar';
 import style from "../Styles/dashboard.module.css"
 import Poste from './poste';
+import NavigateNextOutlinedIcon from '@mui/icons-material/NavigateNextOutlined';
 
 const Dashboards = () => {
     return (
             <div className={style.dashboard} >
                 <Sidebar/>
-                <div className={style.postes}>
-                    <Poste intitule="ReactJs/Nodj Developer "
-                           departement="Departement IT"
-                           lieu ="Casablanca, avenu des far 44" 
-                           contrat="CDI"
-                           nbrcandidat="123"
-                           etat="Ferme"
-                           nbrprofil="50"
-                    />
-                    <Poste intitule="ReactJs/Nodj Developer "
-                           departement="Departement IT"
-                           lieu ="Casablanca, avenu des far 44" 
-                           contrat="CDI"
-                           nbrcandidat="123"
-                           etat="Ferme"
-                           nbrprofil="50"
-                    />
-                    <Poste intitule="ReactJs/Nodj Developer "
-                           departement="Departement IT"
-                           lieu ="Casablanca, avenu des far 44" 
-                           contrat="CDI"
-                           nbrcandidat="123"
-                           etat="Ferme"
-                           nbrprofil="50"
-                    />
-                    <Poste intitule="ReactJs/Nodj Developer "
-                           departement="Departement IT"
-                           lieu ="Casablanca, avenu des far 44" 
-                           contrat="CDI"
-                           nbrcandidat="123"
-                           etat="Ferme"
-                           nbrprofil="50"
-                    />
-                    <Poste intitule="ReactJs/Nodj Developer "
-                           departement="Departement IT"
-                           lieu ="Casablanca, avenu des far 44" 
-                           contrat="CDI"
-                           nbrcandidat="123"
-                           etat="Ferme"
-                           nbrprofil="50"
-                    />
-                    <Poste intitule="ReactJs/Nodj Developer "
-                           departement="Departement IT"
-                           lieu ="Casablanca, avenu des far 44" 
-                           contrat="CDI"
-                           nbrcandidat="123"
-                           etat="Ferme"
-                           nbrprofil="50"
-                    />
-                </div>
+                    <div className={style.postes}>
+                        <Poste intitule="ReactJs/Nodj Developer "
+                            departement="Departement IT"
+                            lieu ="Casablanca, avenu des far 44" 
+                            contrat="CDI"
+                            nbrcandidat="123"
+                            etat="Ferme"
+                            nbrprofil="50"
+                        />
+                        <Poste intitule="ReactJs/Nodj Developer "
+                            departement="Departement IT"
+                            lieu ="Casablanca, avenu des far 44" 
+                            contrat="CDI"
+                            nbrcandidat="123"
+                            etat="Ferme"
+                            nbrprofil="50"
+                        />
+                        <Poste intitule="ReactJs/Nodj Developer "
+                            departement="Departement IT"
+                            lieu ="Casablanca, avenu des far 44" 
+                            contrat="CDI"
+                            nbrcandidat="123"
+                            etat="Ferme"
+                            nbrprofil="50"
+                        />
+                        <Poste intitule="ReactJs/Nodj Developer "
+                            departement="Departement IT"
+                            lieu ="Casablanca, avenu des far 44" 
+                            contrat="CDI"
+                            nbrcandidat="123"
+                            etat="Ferme"
+                            nbrprofil="50"
+                        />
+                        <Poste intitule="ReactJs/Nodj Developer "
+                            departement="Departement IT"
+                            lieu ="Casablanca, avenu des far 44" 
+                            contrat="CDI"
+                            nbrcandidat="123"
+                            etat="Ferme"
+                            nbrprofil="50"
+                        />
+                        <Poste intitule="ReactJs/Nodj Developer "
+                            departement="Departement IT"
+                            lieu ="Casablanca, avenu des far 44" 
+                            contrat="CDI"
+                            nbrcandidat="123"
+                            etat="Ferme"
+                            nbrprofil="50"
+                        />
+                        <div className={style.morebutton}> 
+                           <button>Afficher les 6 offres suivantes <NavigateNextOutlinedIcon/>  </button>
+                        </div>
+                   
+                    </div>
+              
+               
             </div>
             );
 }
